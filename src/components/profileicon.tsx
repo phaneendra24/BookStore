58033c78f8ec0c44733ecab2e8569f4a11c1ecf8
phaneendra <phaneendrapilli777@gmail.com>
2023-06-17 13:01:18 +0530

    removing discrod proverides

--- src/components/profileicon.tsx
+++ src/components/profileicon.tsx
@@ -1,11 +1,14 @@
-import { useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 
 export default function Profileicon() {
   const { data } = useSession();
 
   return (
-    <div className="h-fit w-fit cursor-pointer rounded-full border-[2px] border-green-900">
+    <div
+      className="h-fit w-fit cursor-pointer rounded-full border-[2px] border-green-900"
+      onClick={() => signIn()}
+    >
       <Image
         src={data?.user.image ?? "/avatar.svg"}
         alt="err loading"
