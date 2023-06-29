603f2c593a495bfdc8f08533bc51f7126cb02f3b
phaneendra <phaneendrapilli777@gmail.com>
2023-06-29 17:36:27 +0530

    async router err

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,12 +1,12 @@
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 
-export default function Signin() {
+export default async function Signin() {
   const { data: session } = useSession();
   const router = useRouter();
 
   if (session) {
-    router.push("/");
+    await router.push("/");
   }
   return (
     <div className="flex h-[60vh] w-full flex-col items-center justify-center">
