890a11b4176b91430f9d3aa45aa93be7eb47b78e
phaneendra <phaneendrapilli777@gmail.com>
2023-07-02 10:39:21 +0530

    dev fixes

--- src/pages/messages.tsx
+++ src/pages/messages.tsx
@@ -1,14 +1,3 @@
-import { GetSessionParams, getSession } from "next-auth/react";
-import { useEffect } from "react";
-
 export default function Messages() {
-  useEffect(() => {
-    const call = async () => {
-      const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-      const data = await res.json();
-      console.log(data);
-    };
-    call();
-  }, []);
   return <>Messages</>;
 }
