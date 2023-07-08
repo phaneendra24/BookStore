832006804534043c8360e0dc7154d1a32bb02d12
phaneendra <phaneendrapilli777@gmail.com>
2023-07-08 11:05:41 +0530

    dev errs

--- src/server/api/routers/sales.ts
+++ src/server/api/routers/sales.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const salesRouter = createTRPCRouter({
-  buyproduct: protectedProcedure.input(z.string()).mutation(async (ctx) => {
+  buyproduct: protectedProcedure.input(z.string()).mutation((ctx) => {
     return null;
   }),
 });
