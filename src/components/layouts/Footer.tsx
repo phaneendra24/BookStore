eaf45bca74f33bc46fc203c2f68bcb0195d0c72e
phaneendra <94274447+phaneendra24@users.noreply.github.com>
2023-09-10 14:08:56 +0530

    key err fix

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className="fixed bottom-0 flex h-14 w-full items-center  justify-around bg-green-300 sm:hidden">
       {options.map((i) => {
         return (
-          <button>
+          <button key={i.name}>
             <Link href={i.link}>
               <Image src={i.src} alt="img" width={20} height={20} />
             </Link>
